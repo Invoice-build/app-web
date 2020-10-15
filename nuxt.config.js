da9639a5e435d1883f9fa8a15160eede0ab041ce
Gareth Fuller <gareth.a.f@gmail.com>
2020-10-15 19:26:45 +0100

    Lint fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
       { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
       { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
       { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: "#5bbad5" },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
       { rel: 'msapplication-TileColor', content: '#da532c' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: '/css/fontawesome/css/all.min.css' }
