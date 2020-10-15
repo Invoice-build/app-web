74e57dc464104da982743a717dfca828e393ddb9
Gareth Fuller <gareth.a.f@gmail.com>
2020-10-15 19:20:33 +0100

    Cleanup config & add favicon links

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,34 +2,16 @@ import nuxtI18nConfig from './lib/nuxt-i18n-config'
 import { title, description, ogMeta } from './lib/meta'
 
 export default {
-  /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
-  mode: 'spa',
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
   target: 'server',
-  /*
-  ** Customize the progress-bar color
-  */
+
   loading: false,
-  /*
-  ** Nuxt ENV
-  ** See https://nuxtjs.org/blog/moving-from-nuxtjs-dotenv-to-runtime-config/
-  */
+
   publicRuntimeConfig: {
     APP_ENV: process.env.APP_ENV || 'development',
     APP_DOMAIN: process.env.APP_DOMAIN || 'localhost:3000',
     API_URL: process.env.API_URL || 'http://localhost:5000'
   },
-  privateRuntimeConfig: {},
-  /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+
   head: {
     htmlAttrs: {
       prefix: 'og: http://ogp.me/ns#'
@@ -42,6 +24,12 @@ export default {
       ...ogMeta
     ],
     link: [
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: "#5bbad5" },
+      { rel: 'msapplication-TileColor', content: '#da532c' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: '/css/fontawesome/css/all.min.css' }
     ],
@@ -50,16 +38,11 @@ export default {
       { src: 'https://squid.invoice.build/script.js', site: process.env.FATHOM_SITE_ID, defer: true }
     ]
   },
-  /*
-  ** Global CSS
-  */
+
   css: [
     '~/assets/css/app.css'
   ],
-  /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
+
   plugins: [
     '~/plugins/axios',
     '~/plugins/globals',
@@ -67,34 +50,20 @@ export default {
     '~/plugins/utils',
     '~/plugins/breakpoints'
   ],
-  /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
+
   components: [
     '~/components/global'
   ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
+
   modules: [
-    // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     ['nuxt-i18n', nuxtI18nConfig]
   ],
-  /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
-  }
+
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss'
+  ],
+
+  build: {}
 }
