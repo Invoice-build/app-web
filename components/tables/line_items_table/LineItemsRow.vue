577f39687853bedbd29e52a87f5277960a94bfeb
Gareth Fuller <gareth.a.f@gmail.com>
2020-11-03 19:15:09 +0000

    Make all lineitem rows required

--- components/tables/line_items_table/LineItemsRow.vue
+++ components/tables/line_items_table/LineItemsRow.vue
@@ -1,50 +1,50 @@
 <template>
   <tr>
-    <td :class="['text-left p-2', { 'bg-orange-100': (rowIndex === 0 && !item.description) }]">
+    <td :class="['text-left p-2', { 'bg-orange-100': (rowIndex % 2 == 0 && !item.description) }]">
       <base-text-input
         v-model="item.description"
         placeholder="Item details..."
-        :required="rowIndex == 0"
-        :rules="rowIndex == 0 ? [isRequired()] : []"
+        :rules="[isRequired()]"
         :disabled="!editable"
         :mb="0"
+        required
         absolute-error
       />
     </td>
-    <td :class="['text-right p-2', { 'bg-orange-100': (rowIndex === 0 && !item.quantity) }]">
+    <td :class="['text-right p-2', { 'bg-orange-100': (rowIndex % 2 == 0 && !item.quantity) }]">
       <base-text-input
         v-model="item.quantity"
         placeholder="e.g. 1   "
-        :required="rowIndex == 0"
         :rules="quantityRules"
         :disabled="!editable"
         :mb="0"
         absolute-error
         align-right
+        required
       />
     </td>
-    <td :class="['text-left p-2', { 'bg-orange-100': (rowIndex === 0 && !item.quantity_type) }]">
+    <td :class="['text-left p-2', { 'bg-orange-100': (rowIndex % 2 == 0 && !item.quantity_type) }]">
       <base-select
         v-model="item.quantity_type"
         :options="quantityTypes"
         placeholder="Choose..."
-        :required="rowIndex == 0"
-        :rules="rowIndex == 0 ? [isRequired()] : []"
+        :rules="[isRequired()]"
         :disabled="!editable"
         :mb="0"
         absolute-error
+        required
         class="w-32"
       />
     </td>
-    <td :class="['text-right p-2', { 'bg-orange-100': (rowIndex === 0 && !item.unit_price) }]">
+    <td :class="['text-right p-2', { 'bg-orange-100': (rowIndex % 2 == 0 && !item.unit_price) }]">
       <base-text-input
         v-model="item.unit_price"
         placeholder="Price/unit...   "
-        :required="rowIndex == 0"
         :rules="unitPriceRules"
         :disabled="!editable"
         align-right
         absolute-error
+        required
         :mb="0"
       />
     </td>
@@ -83,15 +83,11 @@ export default {
     },
 
     quantityRules () {
-      const rules = [isNumber(), isLessThanOrEqualTo(this.maxQuantity), maxDecimals(1), isPositive()]
-      if (this.rowIndex === 0) rules.unshift(isRequired())
-      return rules
+      return [isRequired(), isNumber(), isLessThanOrEqualTo(this.maxQuantity), maxDecimals(1), isPositive()]
     },
 
     unitPriceRules () {
-      const rules = [isNumber(), isLessThanOrEqualTo(this.maxUnitPrice), maxDecimals(6), isPositive()]
-      if (this.rowIndex === 0) rules.unshift(isRequired())
-      return rules
+      return [isRequired(), isNumber(), isLessThanOrEqualTo(this.maxUnitPrice), maxDecimals(6), isPositive()]
     }
   },
 
