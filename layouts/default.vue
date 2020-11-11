c743f1c9b36ca111ec6bedb050e7452c3da4ff8b
Gareth Fuller <gareth.a.f@gmail.com>
2020-11-11 13:53:20 +0000

    Remove provider setting from layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,6 @@
 </template>
 
 <script>
-import Web3 from 'web3'
 import { mapActions } from 'vuex'
 import AppNav from '~/components/navs/AppNav.vue'
 import AppSnackbars from '~/components/alerts/AppSnackbars.vue'
@@ -27,7 +26,6 @@ export default {
   async beforeMount () {
     try {
       await this.getTokens()
-      this.setProvider()
     } catch (error) {
       console.error(error)
     } finally {
@@ -38,15 +36,7 @@ export default {
   methods: {
     ...mapActions({
       getTokens: 'tokens/get'
-    }),
-
-    setProvider () {
-      if (!Web3.givenProvider) {
-        const network = !this.invoice ? 'mainnet' : this.invoice.network
-        this.$eth.config.provider = new Web3.providers.WebsocketProvider(`wss://${network}.infura.io/ws/v3/${this.$config.INFURA_PROJECT_ID}`)
-        this.$eth.config.web3 = new Web3(this.$eth.config.provider)
-      }
-    }
+    })
   },
 
   head: {
